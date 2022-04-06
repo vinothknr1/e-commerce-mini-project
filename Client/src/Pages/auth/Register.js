dfd03df21f3a6edd14fb37938682ed77d61e4944
sudharsan <sudharsansubash8292@gmail.com>
2022-04-06 21:36:58 +0530

    firebase and register

--- Client/src/Pages/auth/Register.js
+++ Client/src/Pages/auth/Register.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 const Register = () => {
+  const [email,setEmail] = useState('');
+  const handleSubmit = () => {
+
+  }
+  const registerform = () => <form onSubmit={handleSubmit}>
+    <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} autoFocus/>
+    <button type="submit" className="btn btn-raised">Register</button>
+
+  </form>
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerform()}
+        </div>
+
+      </div>
+      
     </div>
   );
 };
---
+++ Client/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { initializeApp } from "firebase/app";
+import * as firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyA3GZuQ-0J8jBAKxaCyM8ebMrpm4P5E4Bs",
+  authDomain: "ecommerce-fc6ab.firebaseapp.com",
+  projectId: "ecommerce-fc6ab",
+  storageBucket: "ecommerce-fc6ab.appspot.com",
+  messagingSenderId: "792375470410",
+  appId: "1:792375470410:web:485f1212c27cfd5224f9b3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
