f3babe734016a8f2b16b330e07961449652a2de2
sudharsan <sudharsansubash8292@gmail.com>
2022-04-11 21:06:27 +0530

    signin with email and password

--- Client/src/Pages/auth/Register.js
+++ Client/src/Pages/auth/Register.js
@@ -26,8 +26,11 @@ const Register = () => {
         className="form-control"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        placeholder="Your e-mail"
         autoFocus
       />
+    <br/>
+
       <button type="submit" className="btn btn-raised">
         Register
       </button>
