58dd0c013a10bc4c6b9a68057f171d860284d657
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-04-14 18:13:16 +0530

    Forgot Password Error Solved

--- Client/src/Pages/auth/Login.js
+++ Client/src/Pages/auth/Login.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { auth, googleAuthProvider } from "../../firebase";
-import { toast } from "react-toastify";
-import { Button } from "antd";
-import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { Login } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from 'react';
+import { auth, googleAuthProvider } from '../../firebase';
+import { toast } from 'react-toastify';
+import { Button } from 'antd';
+import { MailOutlined, GoogleOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+
+import 'react-toastify/dist/ReactToastify.css';
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   let history = useNavigate();
   let dispatch = useDispatch();
@@ -23,13 +23,13 @@ const Login = () => {
       const { user } = result;
       const idTokenResult = await user.getIdTokenResult();
       dispatch({
-        type: "LOGGED IN USER",
+        type: 'LOGGED IN USER',
         payload: {
           email: user.email,
           token: idTokenResult.token,
         },
       });
-      history("/");
+      history('/');
     } catch (error) {
       console.log(error);
       toast.error(error.message);
@@ -43,13 +43,13 @@ const Login = () => {
         const { user } = result;
         const idTokenResult = await user.getIdTokenResult();
         dispatch({
-          type: "LOGGED_IN_USER",
+          type: 'LOGGED_IN_USER',
           payload: {
             email: user.email,
             token: idTokenResult.token,
           },
         });
-        history("/");
+        history('/');
       })
       .catch((err) => {
         console.log(err);
@@ -58,35 +58,35 @@ const Login = () => {
   };
   const loginForm = () => (
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
+      <div className='form-group'>
         <input
-          type="email"
-          className="form-control"
+          type='email'
+          className='form-control'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Your e-mail"
+          placeholder='Your e-mail'
           autoFocus
         />
       </div>
-      <div className="form-group">
+      <div className='form-group'>
         <input
-          type="password"
-          className="form-control"
+          type='password'
+          className='form-control'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Your password"
+          placeholder='Your password'
         />
       </div>
       <br />
 
       <Button
         onClick={handleSubmit}
-        type="primary"
-        className="mb-3"
+        type='primary'
+        className='mb-3'
         block
-        shape="round"
+        shape='round'
         icon={<MailOutlined />}
-        size="large"
+        size='large'
         disabled={!email || password.length < 6}
       >
         Login with E-mail/Password
@@ -95,27 +95,29 @@ const Login = () => {
   );
 
   return (
-    <div className="container p-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
           {loading ? (
-            <h4 className="text-danger">Loading...</h4>
+            <h4 className='text-danger'>Loading...</h4>
           ) : (
             <h4>Login</h4>
           )}
           {loginForm()}
           <Button
             onClick={googleLogin}
-            type="danger"
-            className="mb-3"
+            type='danger'
+            className='mb-3'
             block
-            shape="round"
+            shape='round'
             icon={<GoogleOutlined />}
-            size="large"
+            size='large'
           >
             Login with Google
           </Button>
-          <Link to="/forgot/password" className="float-right text-danger">Forgot Password</Link>
+          <Link to='/forgot/password' className='float-right text-danger'>
+            Forgot Password
+          </Link>
         </div>
       </div>
     </div>
