fd8124f892aef06100d80eb61052bcfdda2691ff
3CSE82 <3CSE82@CSESER.EDU>
2022-04-28 23:50:42 -0700

    Routes folder created

--- Server/Server.js
+++ Server/Server.js
@@ -5,8 +5,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
+const authRoutes=require('./routes/auth')
 
+const app = express();
+app.use("/api",authRoutes)
 mongoose
   .connect(process.env.DATABASE, {})
   .then(() => console.log("DB connected"))
@@ -14,11 +16,4 @@ mongoose
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
-// route
-app.get("/api", (req, res) => {
-  res.json({
-    data: "hey you hit node API updated",
-  });
-});
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log("Server is running on port ${port}"));
+
