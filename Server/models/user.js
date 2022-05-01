1af92a667b202afaa11371699f82b75984a5053e
sudharsan <sudharsansubash8292@gmail.com>
2022-05-01 15:13:23 +0530

    Middleware completed

---
+++ Server/models/user.js
@@ -0,0 +1,26 @@
+const mangoose = require("mangoose");
+const { ObjectId } = mangoose.Schema;
+
+const userSchema = new mangoose.Schema({
+    name:String,
+    email:
+    {
+        type:String,
+        required: true,
+        index: true,
+    },
+    role: {
+        type: String,
+        default: "subscriber",
+
+    },
+    cart: {
+        type: Array,
+        default: [],
+    },
+    address: String,
+   // wishlist:[{type: ObjectId ,ref:"product"}],
+},  
+{ timestamps: true }
+);
+ module.exports = mangoose.module("User" , userSchema);
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
+const { authCheck } = require("../middlewares/auth");
 // import
 const { create0rUpdateUser } = require("../controllers/auth");
-router.get("/create-or-update-user", create0rUpdateUser);
+
+router.post("/create-or-update-user", authCheck , create0rUpdateUser);
+
+
 
 module.exports = router;
